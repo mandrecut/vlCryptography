a8a4c44849596ccc090db0744aa8f5117065ee7f
mandrecut <mircea.andrecut@gmail.com>
2022-11-12 10:26:01 -0700

    Add files via upload

---
+++ src/example.py
@@ -0,0 +1,23 @@
+#(c) Mircea Andrecut,  mircea.andrecut@gmail.com (2022)
+import lorem
+from vlCryptography import vlCryptography
+
+pswd = lorem.sentence()
+print("Pswd:")
+print(pswd,"\n")
+
+plain_text = lorem.paragraph()
+print("Plain text:")
+print(plain_text,"\n")
+
+vlC = vlCryptography()
+
+cypher_text = vlC.crypt(pswd,plain_text)
+print("Cypher text")
+print(cypher_text,"\n")
+
+print("Plain text:")
+plain_text = vlC.crypt(pswd,cypher_text)
+print(plain_text)
+
+
---
+++ src/vlCryptography.py
@@ -0,0 +1,16 @@
+#(c) Mircea Andrecut,  mircea.andrecut@gmail.com (2022)
+class vlCryptography(object):
+    def __init__(self):
+        self._alphabet = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    def crypt(self,pswd,txt):
+        if len(pswd) < 8:
+            raise ValueError("Password must be minimum 8 characters length.")
+        K,M,N = len(self._alphabet),len(pswd),len(txt)
+        pswd = [self._alphabet.index(q) for q in pswd]
+        for J in range(N):
+            if J < M: # mixing
+                pswd = [(pswd[m-2] + pswd[m-1] + pswd[m]) % K for m in range(M)]
+            else: # mixing & growing
+                pswd = [(pswd[m-2] + pswd[m-1] + pswd[m]) % K for m in range(J)]
+                pswd.append(sum(pswd) % K)
+        return "".join([self._alphabet[(pswd[n] - self._alphabet.index(txt[n]))%K] for n in range(N)])
